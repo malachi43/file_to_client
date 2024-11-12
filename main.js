d39dcd820d628fd9c5562775cc935bf28382c5b7
Malachi <ukomalachi@gmail.com>
2024-11-12 11:59:43 +0100

    made changes to writeToHtmlFile

--- main.js
+++ main.js
@@ -3,9 +3,6 @@ const returnJsFileFromDirectory = require("./lib/returnJsFileFromDirectory");
 const createhHtmlFromJsFiles = require("./lib/createHtmlFileFromJsFile");
 const cp = require("node:child_process");
 
-const dir = "./lib"
+const dir = "lib"
 const files = returnJsFileFromDirectory(dir, ".js");
 createhHtmlFromJsFiles(files);
-// const command = "live-server"
-// const args = ["OUTPUT"];
-// const childProcess = cp.spawnSync(command, args, { stdio: "pipe", shell: true });
