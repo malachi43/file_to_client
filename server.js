367beb49e02e9ec959a56e724568aebce86223b8
malachi <ukomalachi@gmail.com>
2025-02-22 13:08:29 +0100

    refactor files in lib folder by changing file names

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // require("dotenv").config();
 const express = require("express");
 const app = express();
+const ngrok = require('@ngrok/ngrok');
 const { existsSync, writeFileSync, unlinkSync } = require("node:fs");
+const isDirectoryEmpty = require("./lib/isDirectoryEmpty");
 let PORT = 4000;
 const path = require("node:path");
 const publicFolder = "FTC_OUTPUT"
@@ -11,18 +13,19 @@ if (existsSync(path.join(process.cwd(), outputFilename))) {
     unlinkSync(path.join(process.cwd(), outputFilename));
 }
 //file to check what user viewed.
-require("./lib/createFile");
+require("./lib/createFTC_OUTPUT_FILE");
 
 if (!existsSync(path.join(process.cwd(), publicFolder))) throw new Error("FTC_OUTPUT not found in current directory.");
 
 
 app.use((req, res, next) => {
-    const outputFileAbsolutePath = path.join(process.cwd(), outputFilename);
-    writeFileSync(outputFilename, `The user viewed ${path.basename(req.url)}\n`, { flag: "a" });
+    if (req.url.includes(".html")) {
+        writeFileSync(outputFilename, `The user viewed ${path.basename(req.url)}\n`, { flag: "a" });
+    }
     next();
 })
 
-app.use("/", express.static(publicFolder));
+app.use("/", express.static(path.join(process.cwd(), publicFolder)));
 
 app.use((req, res) => {
     res.send(`<h3 style="text-align: center">OOPS! - PAGE NOT FOUND 404</h3>`)
@@ -33,8 +36,13 @@ app.use((err, req, res, next) => {
     res.send(`<h3 style="text-align: center">${err.message}</h3>`)
 })
 
+
+if (isDirectoryEmpty(publicFolder)) return
+
 app.listen(PORT, async () => {
     console.log(`server is listening on port ${PORT}`)
+    ngrok.connect({ addr: PORT, authtoken_from_env: true })
+        .then(listener => console.log(`Ingress established at: ${listener.url()}`));
 })
 
 
